eb293b13d2c277b64a9817f8a6e1c0dd1388f93d
pablo <codewithpablo@gmail.com>
2025-06-09 20:41:22 -0500

    provide acces to port api

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -17,6 +17,14 @@ resource "aws_security_group" "nginx-server-sg" {
     cidr_blocks = ["0.0.0.0/0"] # permite el acceso desde cualquier IP
   }
 
+  # permite el acceso al puerto 5000 para la API
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
