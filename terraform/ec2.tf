722f4dae5bd7bcc5332642965c1d3d7249e38a57
pablo <codewithpablo@gmail.com>
2025-06-09 20:06:48 -0500

    add commands to install docker in ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -5,10 +5,18 @@ resource "aws_instance" "nginx-server" {
 
   user_data = <<-EOF
               #!/bin/bash
-              sudo apt update -y
-              sudo apt install -y nginx
-              sudo systemctl enable nginx
-              sudo systemctl start nginx
+              apt-get update -y
+              apt-get install -y docker.io git
+              systemctl start docker
+              systemctl enable docker
+
+              # Clona el repositorio
+              git clone https://github.com/codeTePablo/JenkisAPI.git /app
+
+              # Construye y corre el contenedor
+              cd /app
+              docker build -t mi-api .
+              docker run -d -p 5000:5000 --name contenedor-api mi-api
               EOF
 
   key_name = aws_key_pair.nginx-server-ssh.key_name # conecta la key creada
